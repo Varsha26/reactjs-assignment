606875609bc7af73900c4698e0e23f7cfd549c57
Varsha Rani <varsha.rani1@ibm.com>
2020-08-21 13:12:24 +0530

    Removing unwanter code

--- react-assignment/src/App.js
+++ react-assignment/src/App.js
@@ -1,15 +1,12 @@
-import React, { Fragment, useEffect } from "react";
-import About from './components/About';
+import React, {useEffect } from "react";
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
-import Teams from './components/Teams';
-import Dash from './components/Dash';
 import Alert from "./components/Alert";
 import { Provider } from "react-redux";
 import store from "./store";
 import setAuthToken from "./utils/setAuthToken";
 import {loadUser} from "./actions/auth";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -22,35 +19,11 @@ function App() {
     <Provider store={store}>
           <Router>
             <div>
+              <Alert />
               <Route path="/login" component={Login} />
               <Route path="/dashboard" component={Dashboard} />
             </div>
           </Router>
-          {/* <Router>
-            <div>
-              <Route path="/login" component={Login} />
-              <Dashboard />
-              <Switch>
-                <Route exactly component={Dash} pattern="/dash" />
-                <Route exactly component={About} pattern="/about" />
-                <Route exactly component={Teams} pattern="/teams" />
-              </Switch>
-            </div>
-          </Router> */}
-      {/* <Router>
-        <Fragment>
-          <Route exact path="/" component={Login} />
-          <section className="App">
-            <Alert />
-            <Switch>
-              <Route exact path="/login" component={Login} />
-              <Route exact path='/dashboard' component={Dashboard} />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/teams' component={Teams} />
-            </Switch>
-          </section>
-        </Fragment>
-      </Router> */}
     </Provider>
     
   );
